20edc80d9410032bd516fb68755dc0ed32f0403d
officialf1zzy <fuzzt000@gmail.com>
2026-04-26 18:48:20 +0200

    Delete src directory

--- src/kernel/string.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-void *memset(void *s, int c, size_t n) {
-    unsigned char *p = s;
-    for (size_t i = 0; i < n; i++) {
-        p[i] = (unsigned char)c;
-    }
-    return s;
-}
