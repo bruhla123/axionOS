0e755f34e75a7dc4551108451c1a9a168d2eac9c
bruhla123 <vidi0611@skovde.se>
2026-05-07 10:51:30 +0200

    Update game.c

--- src/userland/game.c
+++ src/userland/game.c
@@ -1,6 +1,17 @@
 #include "../drivers/vga.h"
 #include "../drivers/keyboard.h"
 
+int strcmp(const char *a, const char *b)
+{
+    while (*a && *b) {
+        if (*a != *b)
+            return *a - *b;
+        a++;
+        b++;
+    }
+    return *a - *b;
+}
+
 void start() {
   printc("starting game");
 }
